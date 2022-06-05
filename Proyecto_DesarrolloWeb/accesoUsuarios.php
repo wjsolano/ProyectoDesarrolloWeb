234a5f077fc9bbb3d1ed7551aa66471bd6676cbc
Hector858 <hicedeno@espe.edu.ec>
2022-06-05 15:56:08 -0500

    creacion del login

---
+++ Proyecto_DesarrolloWeb/accesoUsuarios.php
@@ -0,0 +1,48 @@
+<?php
+//recibir datos desde el formulario
+$user=$_POST['username'];
+$pass=$_POST['password'];
+
+//me permite verificar si es que tiene un valor
+//dentro de user
+if(isset($user)){
+    //Conexión a la base datos
+    //constantes de conexion
+    define('SERVERNAME','localhost');
+    define('USERNAME', 'root');
+    define('PASSWORD', '');
+    define('DBNAME', 'biblioteca');
+
+    //conexion a la base de datos
+    $conn=mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DBNAME) or
+    die("Error en la conexión");
+
+    //iniciar sesión
+    session_start();
+
+    //consultar si los datos estan en la base
+    $query="SELECT * FROM biblioteca WHERE username='$user' AND
+    password='$pass'";
+
+    //ejecutar la consulta
+    $resultado=mysqli_query($conn, $query) or 
+    die(mysqli_query_errono());
+
+    //almacenar el/los datos en un arreglo y tomo el siguiente
+    $fila=mysqli_fetch_array($resultado);
+
+    //controlar si en verad llegan datos
+    if($fila['id_log']==null){
+        //redirigir el mismo index
+        header("location:index.html");
+    }else{
+        //definimos las variables de sesion y reedirigimos a la
+        //pagina de usuario
+        $_SESSION['id']=$fila['id_log'];
+        $_SESSION['nombre']=$fila['nombre'];
+        header("location:paginaUsuario.php");
+    }
+}else{
+    header("location:index.html");
+}
+?>
