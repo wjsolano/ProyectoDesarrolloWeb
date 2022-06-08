bfef9c11af3b3eb445ea53a95b933c0749a3ef88
jordan07espi <103512603+jordan07espi@users.noreply.github.com>
2022-06-07 23:00:04 -0500

    conexión de base de datos y estructura de las tablas en libroAdmin

--- Proyecto_DesarrolloWeb/librosAdmin.php
+++ Proyecto_DesarrolloWeb/librosAdmin.php
@@ -1,14 +1,82 @@
+<?php
+require_once 'conexion.php'; /*TRAER LA CONEXION */
+//consulta de los datos
+
+$query = "SELECT id_usuario,nombre,apellido,direccion,ciudad,telefono,cedula,tipo_usuario,username FROM usuario";
+//ejecutar consulta
+$result= $conexion -> query($query); //todos los campos de la consulta
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Libros Admin</title>
+    <link rel="stylesheet" href="estilos/estilos.css">
+    <style>
+        table, th, td{
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+
+        th, td{
+            padding: 5px;
+            text-align: left;
+        }
+    </style>
+    <title>Index</title>
 </head>
+<script type="text/javascript">
+    function ConfirmDelate(){
+        var respuesta = confirm("¿De verdad desea eliminar el cliente?");
+        if(respuesta==true){
+            return true;
+        }else{
+            return false;
+        }
+    }
+</script>
 <body>
-    <TAble>
-        <H1>Lista e libros Admin</H1>
-    </TAble>
+    <div>
+        <h2> Clientes</h2>   
+        <div id="tabla-contenedor">
+        <table border="2" class="table table-fixed">
+            <thead>
+                <tr>
+                    <th id="cabecera">#</th>
+                    <th id="cabecera">Nombre</th>
+                    <th id="cabecera">Apellido</th>
+                    <th id="cabecera">Dirección</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                if($result-> num_rows>0){
+                    //si tiene resultado hacer esto
+                    while($row=$result-> fetch_assoc()){ //el resultado se transforme en matriz asociativa
+                        // 'idusuario'= 1, 'nombreusuario'=luis
+                        echo '<tr>'; //el unto es para oncatenar
+                        echo '<td>' . $row['id_usuario'] .'</td>'; 
+                        echo '<td>' . $row['nombre']  . '</td>'; 
+                        echo '<td>' . $row['apellido'] .'</td>'; 
+                        echo '<td>' . $row['direccion'] .'</td>'; 
+                        echo '<td>';
+                        echo '<a id="loadMore" href="admin/leerPerfilAdmin.php?id='.$row['id_usuario'].'">Leer</a>'; //para pasar es el ? segun donde de click
+                        echo '<a id="loadMore" href="admin/actualizarUsuarios.php?id='.$row['id_usuario'].'">Editar</a>';
+                        echo '<a id="loadMore" href="eliminarPerfilAdmin.php?id='.$row['id_usuario'].'" onclick="return ConfirmDelate()">Eliminar</a>'; 
+                        echo '</td>';
+                        echo '</tr>';
+                    }
+                }else{
+                    echo '<p><em>No existen datos registrados</em></p>';
+                }
+                ?>
+            </tbody>
+        </table>
+        </div>
+        <a href="bienvenidaAdmin.html" id="loadMore">Regresar</a>
+    </div>
 </body>
+<link rel="stylesheet" href="css/estiPerfil.css">
 </html>
